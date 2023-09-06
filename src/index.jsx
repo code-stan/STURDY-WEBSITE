cba3394b5276865fca0949b520e521db3e86a7f7
CODE-STAN <thecoderstanley@gmail.com>
2023-09-06 21:37:04 +0100

    little cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import ReactDom from 'react-dom/client';
 import App from './Components/App';
-// import Preloader from './Components/Pages/Preloader/Preloader';
 import "./Styles/style.scss"
 
 const root = ReactDom.createRoot(document.querySelector("#root"));
